4892e3942879981c4b356f6ae3eb421b8060b36c
etiiizel <franciscoetizel@gmail.com>
2025-02-17 16:18:27 -0300

    index.css-part2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logoSpotfy from '../assets/logo/spotify-logo.png';
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
       <img src={logoSpotfy} alt="Logo do Spotify" />
       <a href="/">
         <h1>Spotfy</h1>
