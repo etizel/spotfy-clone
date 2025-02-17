663099065b46395197d9f0499b177e8457c28eb2
etiiizel <franciscoetizel@gmail.com>
2025-02-17 18:44:28 -0300

    main__texts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,12 @@
 const Main = () => {
-  return <div className="Main">main</div>;
+  return (
+    <div className="main">
+      <div className="main__texts">
+        <h2>Artistas Populares</h2>
+        <a href="/">Mostrar Tudo</a>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
