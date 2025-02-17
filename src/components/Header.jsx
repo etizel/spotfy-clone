63cd25dc138271f5e14fe2307511bb01c82abfda
etiiizel <franciscoetizel@gmail.com>
2025-02-17 13:44:16 -0300

    reload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
+import logoSpotfy from '../assets/logo/spotify-logo.png';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img src={logoSpotfy} alt="Logo do Spotify" />
+      <a href="/">
+        <h1>Spotfy</h1>
+      </a>
+    </div>
+  );
 };
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import Header from './components/Header';
 
 createRoot(document.getElementById('root')).render(<Header />);
